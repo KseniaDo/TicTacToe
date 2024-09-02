d7b9d235dbd740b3facf4c18031fc068448904e2
KseniaDo <54909463+KseniaDo@users.noreply.github.com>
2024-09-02 20:50:36 +0300

    Working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      { value }
+    </button>
   );
 }
 
-export default App;
+function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  function getRow(i) {
+    // const squareHTML = []; 
+    // for (let j=i; j< i+3; j++) {
+    //   squareHTML.push(getElem(j));
+    // }
+    return [...Array(3).keys()].map((i) => {
+      <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)}/>
+    })
+  }
+
+  function getElem(i) {
+    return (
+      <Square value={squares[i]} onSquareClick={() => handleClick(i)}/>
+    )
+  }
+
+  return (
+    <>
+    <div className='status'>{ status }</div>
+    <div className='board-row'>{() => getRow(0)}</div>
+    <div className='board-row'>{() => getRow(3)}</div>
+    <div className='board-row'>{() => getRow(6)}</div>
+    {/* <div className='board-row'>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    </div>
+    <div className='board-row'>
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+    </div>
+    <div className='board-row'>
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>  
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>  
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>      
+    </div> */}
+    </>
+  );
+}
+
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    if (move === currentMove) {
+      return (
+        <li key={move}>
+          { `You are at move #${move}` }
+        </li>
+      );
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{ description }</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>{ moves }</ol>
+      </div>
+    </div>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
